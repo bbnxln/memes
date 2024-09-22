73b25aa31c8cfcd49772114f693a4a69d061ba81
Костина Анастасия <akostina2007@mail.ru>
2024-09-22 18:05:36 +0300

    bogdan lox

---
+++ show_meme.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    image_path = 'memes/meme.jpeg'
+    img = Image.open(image_path)
+
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
